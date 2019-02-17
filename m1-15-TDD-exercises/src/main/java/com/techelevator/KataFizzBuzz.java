8c9b083760ca58594095de7f351fc9a1adfc0808
Daniel <saltzman_daniel@yahoo.com>
2019-02-17 18:39:38 -0500

    finished File IO part 2 exercises

--- m1-15-TDD-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
+++ m1-15-TDD-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,22 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
-
+	
+	public String fizzBuzz(int x) {
+		if(x < 1 || x > 100) {
+			return "";
+		}
+		else if((x % 3 == 0 && x % 5 == 0)) {
+			return "FizzBuzz";
+		} 
+		else if(x % 5 == 0) {
+			return "Buzz";
+		}
+		else if(x % 3 == 0) {
+		 return "Fizz";
+		}
+		else {
+			return ((Integer)x).toString();
+		}
+	}
 }
--- m1-17_FileIO_Writing_out-exercises/src/main/java/com/techelevator/FizzWriter.java
+++ m1-17_FileIO_Writing_out-exercises/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,44 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+
 public class FizzWriter {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		
+		String fizzBuzzFile = "FizzBuzz.txt";
+		File newFile = new File(fizzBuzzFile);
+		newFile.createNewFile();
+		
+		try(PrintWriter writer = new PrintWriter(newFile);
+			BufferedWriter buffered= new BufferedWriter(writer)){	
+		String result = "";
+		for (int x = 1; x <=300; x++) {
+			
+			if(x > 300) {
+				result = "";
+			}
+			else if ( x == 0) {
+				result = "";
+			}
+			else if((x % 3 == 0 && x % 5 == 0)) {
+				result = "FizzBuzz";
+			} 
+			else if(x % 5 == 0) {
+				result = "Buzz";
+			}
+			else if(x % 3 == 0) {
+			 result = "Fizz";
+			}
+			else {
+				result = ((Integer)x).toString();
+		}
+			buffered.write(result + "\n");
+		}
+		}	
 	}
-
 }
